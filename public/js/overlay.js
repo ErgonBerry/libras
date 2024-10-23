b83c7526d4ef97ecc5dbd5596d15bca1cb4bcd4d
dodopontocom <rodolfotiago@gmail.com>
2024-10-23 11:14:17 -0300

    remove unused

--- public/js/overlay.js
+++
@@ -1,7 +0,0 @@
-// overlay.js
-document.addEventListener("DOMContentLoaded", function() {
-    const overlayText = document.createElement("div");
-    overlayText.id = "overlay-text";
-    overlayText.innerText = "Texto Sobreposto"; // Altere aqui se quiser outro texto
-    document.body.appendChild(overlayText);
-});
